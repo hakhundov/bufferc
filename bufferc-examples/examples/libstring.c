670b342730457fa1b909bae18674dfe8e98cd3e5
Haji Akhundov <hajiakhundov@gmail.com>
2016-02-03 16:59:45 +0100

    update gitignore, untrack .c file

--- bufferc-examples/examples/libstring.c
+++ bufferc-examples/examples/libstring.c
@@ -4,45 +4,41 @@
 #include "libstring.h"
 	
 	
-	int chr_is_space(char c) {
-	return ((c == '\32') || (c == '\t'));
-	Return("final"())
-	}
-	
 	buffer * str_copy(buffer * dst, buffer * src) {
 	
-	for(int i = 0; (i <  buf_length(src)); i++) {
+	for(int i = 0; (i <  buf_size(src)); i++) {
 			
-			if((i <  buf_length(dst))) {
+			if((i <  buf_size(dst))) {
 			  assign_char(dst, i, read_element(src, i));
 			}
 			
 		}
 	
 	return dst;
-	Return("final"())
 	}
 	
 	buffer * str_clone(buffer * src) {
-	buffer * dst = alloc_buf(buf_length(src));
+	buffer * dst = alloc_buf(buf_size(src));
 	return str_copy(dst, src);
-	Return("final"())
 	}
 	
 	buffer * str_concat(buffer * s1, buffer * s2) {
-	buffer * dst = alloc_buf((buf_length(s1) + buf_length(s2)));
+	buffer * dst = alloc_buf((buf_size(s1) + buf_size(s2)));
 	str_copy(dst, s1);
 	
-	for(int i = buf_length(s1); (i <  buf_length(dst)); i++) {
+	for(int i = buf_size(s1); (i <  buf_size(dst)); i++) {
 			
-			if(((i - buf_length(s1)) <  buf_length(s2))) {
-			  assign_char(dst, i, read_element(s2, (i - buf_length(s1))));
+			if(((i - buf_size(s1)) <  buf_size(s2))) {
+			  assign_char(dst, i, read_element(s2, (i - buf_size(s1))));
 			}
 			
 		}
 	
 	return dst;
-	Return("final"())
+	}
+	
+	int chr_is_space(char c) {
+	return ((c == '\32') || (c == '\t'));
 	}
 	
 	buffer * str_trim(buffer * s) {
@@ -55,30 +51,29 @@
 		pos++;
 	}
 	
-	pos = (buf_length(s) - 1);
+	pos = (buf_size(s) - 1);
 	
 	while(chr_is_space(read_element(s, pos))) {
 		numTrailingSpaces++;
 		pos--;
 	}
 	
-	buffer * dst = alloc_buf(((buf_length(s) - numLeadingSpaces) - numTrailingSpaces));
+	buffer * dst = alloc_buf(((buf_size(s) - numLeadingSpaces) - numTrailingSpaces));
 	
-	for(int i = 0; (i <  buf_length(dst)); i++) {
+	for(int i = 0; (i <  buf_size(dst)); i++) {
 			assign_char(dst, i, read_element(s, (i + numLeadingSpaces)));
 		}
 	
 	return dst;
-	Return("final"())
 	}
 	
 	int str_equal(buffer * first, buffer * second) {
 	
-	if((buf_length(first) != buf_length(second))) {
+	if((buf_size(first) != buf_size(second))) {
 	  return 0; 
 	} else {
 		
-		for(int i = 0; (i <  buf_length(first)); i++) {
+		for(int i = 0; (i <  buf_size(first)); i++) {
 				
 				if((read_element(first, i) != read_element(second, i))) {
 				  return 0;
@@ -88,31 +83,29 @@
 		
 	}
 	return 1;
-	Return("final"())
+	}
+	
+	int min(int a, int b) {
+	if((a <  b)) return a; else return b;
 	}
 	
 	int str_compare(buffer * s1, buffer * s2) {
+	if(str_equal(s1, s2)) return 0;
 	int diff = 0;
 	
-	for(int i = 0; (i <  buf_length(s1)); i++) {
-			
-			if((i <  buf_length(s2))) {
-			  diff = 0;
-			  if((diff >  0)) { return 1; } else 
-			                                if((diff <  0)) {
-			                                  return -1;
-			                                }
-			                                
-			}
-			
+	for(int i = 0; (i <  min(buf_size(s1), buf_size(s2))); i++) {
+			diff = (read_element(s1, i) - read_element(s2, i));
+			if((diff >  0)) { return 1; } else 
+			                              if((diff <  0)) {
+			                                return -1;
+			                              }
+			                              
 		}
 	
 	return 0;
-	Return("final"())
 	}
 	
 	void str_error(buffer * msg) {
-	buffer_printf("Omg errors: %s", msg);
-	Return("final"())
+	printf("ERROR: %s", msg->ptr);
 	}
 	
